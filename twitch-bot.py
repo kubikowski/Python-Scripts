c62df428140c7ecf54f3bbc01e03b813a2dce511
kubikowski <kubikowski0@gmail.com>
2020-07-27 23:03:10 -0400

    This is an old, unfinished problem. But I will push it anyways

---
+++ twitch-bot.py
@@ -0,0 +1,22 @@
+"""
+Name: Twitch Bot
+Written By: Nathaniel Holden
+Date: 3/21/2020
+Dependencies: keyboard
+
+Inputs: a string
+Outputs: writes out a string enery n seconds
+"""
+
+
+import keyboard as kb;
+import asyncio;
+
+def interrupt(text):
+    print(text);
+    kb.write(text);
+
+if __name__ == '__main__':
+    inputStr = str(input('Enter some text: '));
+
+    loop = asyncio.new_event_loop();
