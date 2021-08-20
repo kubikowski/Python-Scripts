cfbea37a9fd5edcf7f13607c3fb22f24b8522484
kubikowski <kubikowski0@gmail.com>
2021-08-20 12:58:31 -0600

    refactored `image_join` into functions

--- image-join.py
+++ image-join.py
@@ -9,35 +9,71 @@ Outputs: a jpg of the images stacked horizontally or vertically
 Notes: may not currently account for image directory. 
 """
 
-import numpy as np
+from typing import Final, Optional
+
+from numpy import hstack, sum, vstack
 from PIL import Image
+
+
+def get_image_input() -> Optional[str]:
+    file_name: Final[str] = input('Image Name or (Stop): ')
+
+    if file_name.lower() == 'stop':
+        return None
+
+    if file_name.endswith('.jpg'):
+        return file_name
+
+    return file_name + '.jpg'
+
+
+def get_images_input() -> list[Image]:
+    file_names: Final[list[str]] = []
+
+    file_name: Optional[str] = get_image_input()
+    while file_name is not None:
+        file_names.append(file_name)
+        file_name = get_image_input()
+
+    return [Image.open(i) for i in file_names]
+
+
+def get_min_image_size(images: list[Image]) -> tuple[int, int]:
+    return sorted([(sum(i.size), i.size) for i in images])[0][1]
+
+
+def resize_images(images: list[Image]) -> list[Image]:
+    min_size: Final[tuple[int, int]] = get_min_image_size(images)
+
+    return [image.resize(min_size) for image in images]
+
+
+def stack_images(images: list[Image]) -> Image:
+    orientation: str = input('(h)orizontal or (v)ertical image stack? ').lower()
+
+    if orientation == 'h':  # horizontal stacking
+        return Image.fromarray(hstack(images))
+
+    if orientation == 'v':  # vertical stacking
+        return Image.fromarray(vstack(images))
+
+
+def save_image(image: Image) -> None:
+    output_file_name: str = input('New Image Name: ')
+    image.save(output_file_name + '.jpg')
+
 
 # This is a tool to join multiple images in an horizontal or vertical stack
 # Just input the image names when requested, choose a direction, and
 # choose an output name
 # you don't need to add the .jpg to the input or output file names
+def join_images() -> None:
+    input_images: Final[list[Image]] = get_images_input()
+    resized_images: Final[list[Image]] = resize_images(input_images)
+    stacked_image: Final[Image] = stack_images(resized_images)
+    save_image(stacked_image)
 
-file_names = []
-while True:
-    file_name: str = str(input("Image Name or (Stop): "))
-    if file_name.lower() == "stop":
-        break
-    file_names.append(file_name + ".jpg")
 
-images: list = [Image.open(i) for i in file_names]
-
-# pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape)
-min_shape: int = sorted([(np.sum(i.size), i.size) for i in images])[0][1]
-
-option: str = input("(h)orizontal or (v)ertical image stack? ")
-
-if option == 'h':  # horizontal stacking
-    image_matrix = np.hstack((np.asarray(i.resize(min_shape)) for i in images))
-    combined_image = Image.fromarray(image_matrix)
-
-if option == 'v':  # vertical stacking
-    image_matrix = np.vstack((np.asarray(i.resize(min_shape)) for i in images))
-    combined_image = Image.fromarray(image_matrix)
-
-output_file_name = input("New Image Name: ")
-combined_image.save(output_file_name + ".jpg")
+if __name__ == '__main__':
+    while True:
+        join_images()
