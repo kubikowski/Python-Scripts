8c06bb9a0556595a717226b4b06d43860dfcd720
kubikowski <kubikowski0@gmail.com>
2021-08-20 19:11:10 -0600

    moved `input_color` logic onto `PixelColor`

--- impose_color/_pixel_color.py
+++ impose_color/_pixel_color.py
@@ -4,7 +4,7 @@ from PIL import ImageColor
 
 
 class PixelColor(object):
-    def __init__(self, color: str):
+    def __init__(self, color: str) -> None:
         super(PixelColor, self).__init__()
         self.color: Final[tuple[int, int, int, int]] = ImageColor.getrgb(color)
         self.r: Final[int] = self.color[0]
@@ -12,16 +12,36 @@ class PixelColor(object):
         self.b: Final[int] = self.color[2]
         self.a: Final[int] = self.color[3] if len(self.color) == 4 else 255
 
+    @staticmethod
+    def input_background_color() -> 'PixelColor':
+        background_color: Final[str] = input(
+            'Please enter a background color:\n' +
+            '  · any web named color, eg: white\n' +
+            '  · any hex color, eg: #FFF or #FFFFFF\n' +
+            '  · any rbg color, eg: rgb(255,255,255)\n' +
+            '  -> ')
+        return PixelColor(background_color)
+
+    @staticmethod
+    def input_foreground_color() -> 'PixelColor':
+        foreground_color: Final[str] = input(
+            'Please enter a foreground color with an alpha value:\n' +
+            '  · any hexa color, eg: #0007 or #0000007F\n' +
+            '  · any rgba color, eg rgba(0,0,0,127)\n' +
+            '  -> ')
+        return PixelColor(foreground_color)
+
     def get_imposed_color(self, imposed: 'PixelColor') -> 'PixelColor':
-        r: Final[int] = self.calculate_target_hue(self.r, imposed.r, imposed.a)
-        g: Final[int] = self.calculate_target_hue(self.g, imposed.g, imposed.a)
-        b: Final[int] = self.calculate_target_hue(self.b, imposed.b, imposed.a)
+        r: Final[int] = self._calculate_target_hue(self.r, imposed.r, imposed.a)
+        g: Final[int] = self._calculate_target_hue(self.g, imposed.g, imposed.a)
+        b: Final[int] = self._calculate_target_hue(self.b, imposed.b, imposed.a)
+
         return PixelColor('rgb({},{},{})'.format(r, g, b))
 
     @staticmethod
-    def calculate_target_hue(bg_hue: int, fg_hue: int, a: int) -> int:
+    def _calculate_target_hue(background_hue: int, foreground_hue: int, a: int) -> int:
         alpha: Final[float] = float(a / 255)
-        return int(((1 - alpha) * bg_hue) + (alpha * fg_hue))
+        return int(((1 - alpha) * background_hue) + (alpha * foreground_hue))
 
     def to_string(self) -> str:
         if len(self.color) == 4:
--- impose_color/impose_color.py
+++ impose_color/impose_color.py
@@ -1,7 +1,7 @@
 """
 Name: Impose Color
 Author: Nathaniel Holden
-Version: 0.1.1
+Version: 0.1.2
 Date: 18/03/2020
 Dependencies: Pillow
 
@@ -18,23 +18,15 @@ from _pixel_color import PixelColor
 
 
 def impose_color():
-    background_input: Final[str] = str(input(
-        'Please enter a background color:\n' +
-        ' · any standard color name, Ex: white\n' +
-        ' · a custom color, EX: #FFF or rgb(255,255,255)\n-> '))
-    background_color: Final[PixelColor] = PixelColor(background_input)
-
-    foreground_input: Final[str] = str(input(
-        'Please enter a custom foreground color with an alpha value:\n' +
-        ' · #FFF7 or #FFFFFF7F or rgba(255,255,255,127)\n-> '))
-    foreground_color: Final[PixelColor] = PixelColor(foreground_input)
-
+    background_color: Final[PixelColor] = PixelColor.input_background_color()
+    foreground_color: Final[PixelColor] = PixelColor.input_foreground_color()
     imposed_color: Final[PixelColor] = background_color.get_imposed_color(foreground_color)
 
     print(
         'background color: {}\n'.format(background_color.to_string()) +
         'foreground color: {}\n'.format(foreground_color.to_string()) +
-        'imposed color:    {}\n'.format(imposed_color.to_string()))
+        'imposed color:    {}\n'.format(imposed_color.to_string())
+    )
 
 
 if __name__ == '__main__':
